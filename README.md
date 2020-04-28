b051e041c51cf91d00b77042bb152a1b0f1a575f
Nathaniel Ferguson <nathaniel@humanaturestudios.com>
2020-04-27 23:21:55 -0500

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # GDMJobSearcher
 Searches for job pages off of GameDevMap
+
+
+So the best way I found for being complete in my job search is going to www.gamedevmap.com and clicking a city/state/province and clicking each studio in there and seeing if they have a job page, etc. This takes a long time, so I made it fast.
+
+I wrote a script which takes a location name as input and will read, sort, and open all webpages with job pages (just if they have "jobs", "careers", "join us", or "opportunities" in the HTML, not 100% hit rate but pretty good). It'll open by N at a time (you input what N is) so you're not opening 50+ tabs at once.
+
+Just download the .zip, unzip it, and click the .exe to run it. It'll ask for a place name as input (it MUST be an option on gamedevmap.com, otherwise you get 0 results.) as well as a number for how many tabs you want opened at a time.
+
+Like I said, I can't promise 100% success getting all website with job pages, there's been a few that I've noticed missing, but this will save A LOT of time.
---
+++ Searcher.py
@@ -0,0 +1,91 @@
+from bs4 import BeautifulSoup, SoupStrainer
+import requests
+import httplib2
+import webbrowser
+
+# Input for city/state/province to search
+location = input("Enter City/State/Province (MUST be present on www.gamedevmap.com) or nothing will happen: ")
+
+#Getting how many tabs to open at once, I wanna let the user have control over this in case their rig is really beefy or really weak
+while True:
+    try:
+        STUDIOS_TO_OPEN_AT_ONCE = int(input("How many tabs to open at once (number only). \n WARNING: large numbers could overload your browser, try keeping it under 10 or 11: "))
+        break
+    except:
+        print("not an int.")
+
+jobPage = 1
+nextURL = ""
+studioPages = []
+studioPagesWithJobs = []
+http = httplib2.Http()
+pages = []
+
+# This chunk will look for every link in gamedevmap.com and all the ones starting with 'http:' get added to a list
+# This isn't particularly stable but I found every link to a games studio on the site starts with http: and not https:
+# so it works for a first pass but maybe isn't scalable?? It spooks me
+print("Please wait, could be opening/parsing 100+ pages so it could take a minute...")
+
+while nextURL == "":
+    status, response = http.request("https://www.gamedevmap.com/index.php?location=" + location + "&country=&state=&city=&query=&type=&start=" + str(jobPage) + "&count=100")
+    # Allows us to go to the next job page
+    jobPage += 100
+    nextURL = ""
+    for link in BeautifulSoup(response, parse_only=SoupStrainer('a'), features="html.parser"):
+        if link.has_attr('href'):
+            if link['href'][:5] =="http:":
+                studioPages.append(link)
+            elif link['href'] == "index.php?location=" + location + "&country=&state=&city=&query=&type=&start=" + str(jobPage) + "&count=100":
+                nextURL = link['href']
+            pages.append(link)
+
+        if "zynga" in link:
+            print()
+    if nextURL == "":
+        break
+    else:
+        status, response = http.request("https://www.gamedevmap.com/index.php?location=" + location + "&country=&state=&city=&query=&type=&start=" + str(jobPage) + "&count=100")
+        # Resets nextURL so we loop through the next job page
+        nextURL = ""
+
+
+
+# This block opens all the game job pages
+for studioPage in studioPages:
+    url = studioPage['href']
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    # We try to open the page
+    try:
+        r = requests.get(url, headers=headers)
+
+        #Parsing the page and making it pretty
+        soup = BeautifulSoup(r.text, 'html.parser')
+        prettySoup = soup.prettify().lower()
+
+        #using the keywords "jobs", "join us", "Careers", and "openings" to see if the studio page has that somewhere in the html text
+        if ("jobs" in prettySoup or "join us" in prettySoup
+                or "careers" in prettySoup or "openings" in prettySoup):
+            print("Job page found: " + url)
+            studioPagesWithJobs.append(url)
+        else:
+            print("No jobs found on url: " + url)
+
+    # some pages return exceptions, we print the exception and the URL
+    # kinda defensive, since if we saw something like www.ea.com had an error, then we know there's probably an error with us
+    except requests.exceptions.RequestException as e:
+        print(str(e) + " on url: " + url)
+
+
+print(str(len(studioPagesWithJobs)) + " studios with possible job pages found.")
+if (len(studioPagesWithJobs) == 0):
+    print("Exiting")
+else:
+    #Opens STUDIOS_TO_OPEN_AT_ONCE pages at a time, to prevent crashing a browser by trying to open like 50 tabs
+    openedPages = 0
+    for i in range(len(studioPagesWithJobs)):
+        openedPages += 1
+        webbrowser.open(studioPagesWithJobs[i])
+        print("Opening: " + studioPagesWithJobs[i])
+        if (openedPages % STUDIOS_TO_OPEN_AT_ONCE == 0):
+            input("Press Enter to continue...")
